8364d6c9626ddc58cf6afbd0faa0ea169694d892
Ikbal Mahendra <32354274+ikblmhndra@users.noreply.github.com>
2021-11-16 21:04:53 +0700

    Add files via upload

---
+++ week-4/Tugas - Problem 3.py
@@ -0,0 +1,12 @@
+a = 50.0
+b = 5.5
+print(a)
+print(b)
+a = a/5
+b = b - 0.5
+print("nilai a kedua", a, "tipe datanya adalah", type(a))
+print("nilai b kedua", b, "tipe datanya adalah", type(b))
+a = b / a
+b -= a
+print("nilai a ketiga", a, "tipe datanya adalah", type(a))
+print("nilai b ketiga", b, "tipe datanya adalah", type(a))
